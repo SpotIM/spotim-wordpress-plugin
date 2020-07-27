a6010d2099f1f26349f03be62e56fb59a007a3d8
Abhijit Rakas <abhijit.rakas@rtcamp.com>
2020-07-27 12:26:13 +0530

    Update plugin main file class name

--- spotim-comments.php
+++ spotim-comments.php
@@ -14,137 +14,137 @@
 
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
 
 // Store Plugin version for internal use.
 if ( ! defined( 'OW_VERSION' ) ) {
-    /**
-     * The version of the plugin
-     *
-     * @since  4.5.2
-     */
-    define( 'OW_VERSION', '4.5.2' );
+	/**
+	 * The version of the plugin
+	 *
+	 * @since  4.5.2
+	 */
+	define( 'OW_VERSION', '4.5.2' );
 }
 
 /**
- * WP_SpotIM
+ * WP_OW
  *
  * A general class for Spot.IM comments for WordPress.
  *
  * @since 1.0.2
  */
-class WP_SpotIM {
+class WP_OW {
 
-    /**
-     * Instance
-     *
-     * @since  1.0.2
-     *
-     * @access private
-     * @static
-     *
-     * @var WP_SpotIM
-     */
-    private static $instance;
+	/**
+	 * Instance
+	 *
+	 * @since  1.0.2
+	 *
+	 * @access private
+	 * @static
+	 *
+	 * @var WP_OW
+	 */
+	private static $instance;
 
-    /**
-     * Constructor
-     *
-     * Get things started.
-     *
-     * @since  1.0.2
-     *
-     * @access protected
-     */
-    protected function __construct() {
+	/**
+	 * Constructor
+	 *
+	 * Get things started.
+	 *
+	 * @since  1.0.2
+	 *
+	 * @access protected
+	 */
+	protected function __construct() {
 
-        // Load plugin files
-        $this->load_files();
+		// Load plugin files
+		$this->load_files();
 
-        // Get the Options
-        $this->options = SpotIM_Options::get_instance();
+		// Get the Options
+		$this->options = SpotIM_Options::get_instance();
 
-        // Run the plugin
-        new SpotIM_i18n();
-        new SpotIM_Cron( $this->options );
-        new SpotIM_Feed();
+		// Run the plugin
+		new SpotIM_i18n();
+		new SpotIM_Cron( $this->options );
+		new SpotIM_Feed();
 
-        if ( is_admin() ) {
+		if ( is_admin() ) {
 
-            // Admin Page
-            new SpotIM_Admin( $this->options );
+			// Admin Page
+			new SpotIM_Admin( $this->options );
 
-        } else {
+		} else {
 
-            // Frontend code: embed script, comments template, comments count.
-            new SpotIM_Frontend( $this->options );
+			// Frontend code: embed script, comments template, comments count.
+			new SpotIM_Frontend( $this->options );
 
-        }
+		}
 
-    }
+	}
 
-    /**
-     * Get Instance
-     *
-     * @since  2.0.0
-     *
-     * @access public
-     * @static
-     *
-     * @return WP_SpotIM
-     */
-    public static function get_instance() {
-        if ( is_null( self::$instance ) ) {
-            self::$instance = new self;
-        }
+	/**
+	 * Get Instance
+	 *
+	 * @since  2.0.0
+	 *
+	 * @access public
+	 * @static
+	 *
+	 * @return WP_OW
+	 */
+	public static function get_instance() {
+		if ( is_null( self::$instance ) ) {
+			self::$instance = new self;
+		}
 
-        return self::$instance;
+		return self::$instance;
 
-    }
+	}
 
-    /**
-     * Load plugin files
-     *
-     * @since  4.3.0 The functionality moved to a method.
-     *
-     * @access public
-     *
-     * @return void
-     */
-    public function load_files() {
+	/**
+	 * Load plugin files
+	 *
+	 * @since  4.3.0 The functionality moved to a method.
+	 *
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function load_files() {
 
-        $inc_class_dir = plugin_dir_path( __FILE__ ) . DIRECTORY_SEPARATOR . 'inc' . DIRECTORY_SEPARATOR;
+		$inc_class_dir = plugin_dir_path( __FILE__ ) . DIRECTORY_SEPARATOR . 'inc' . DIRECTORY_SEPARATOR;
 
-        $files = [
-            'helpers/class-spotim-form.php',
-            'helpers/class-spotim-message.php',
-            'helpers/class-spotim-comment.php',
-            'helpers/class-spotim-json-feed.php',
-            'helpers/class-spotim-wp.php',
-            'class-spotim-i18n.php',
-            'class-spotim-import.php',
-            'class-spotim-options.php',
-            'class-spotim-settings-fields.php',
-            'class-spotim-metabox.php',
-            'class-spotim-admin.php',
-            'class-spotim-frontend.php',
-            'class-spotim-feed.php',
-            'class-spotim-cron.php',
-            'spotim-shortcodes.php',
-            'spotim-widgets.php',
-        ];
+		$files = [
+			'helpers/class-spotim-form.php',
+			'helpers/class-spotim-message.php',
+			'helpers/class-spotim-comment.php',
+			'helpers/class-spotim-json-feed.php',
+			'helpers/class-spotim-wp.php',
+			'class-spotim-i18n.php',
+			'class-spotim-import.php',
+			'class-spotim-options.php',
+			'class-spotim-settings-fields.php',
+			'class-spotim-metabox.php',
+			'class-spotim-admin.php',
+			'class-spotim-frontend.php',
+			'class-spotim-feed.php',
+			'class-spotim-cron.php',
+			'spotim-shortcodes.php',
+			'spotim-widgets.php',
+		];
 
-        foreach ( $files as $file ) {
+		foreach ( $files as $file ) {
 
-            $file = $inc_class_dir . $file;
+			$file = $inc_class_dir . $file;
 
-            if ( file_exists( $file ) ) {
-                require_once( $file );
-            }
-        }
+			if ( file_exists( $file ) ) {
+				require_once( $file );
+			}
+		}
 
-    }
+	}
 
 }
 
@@ -153,10 +153,10 @@ class WP_SpotIM {
  *
  * @since 1.0
  *
- * @return WP_SpotIM
+ * @return WP_OW
  */
 function spotim_instance() {
-    return WP_SpotIM::get_instance();
+	return WP_OW::get_instance();
 }
 
 add_action( 'plugins_loaded', 'spotim_instance', 0 );
@@ -167,13 +167,13 @@ add_action( 'plugins_loaded', 'spotim_instance', 0 );
  * @return bool returns true if current site is available on VIP-GO, otherwise false
  */
 function spotim_is_vip() {
-    if ( defined( 'SPOTIM_IS_VIP_DEBUG' ) && SPOTIM_IS_VIP_DEBUG ) { // Setting WPCOM_IS_VIP_ENV in local won't work.
-        return true;
-    }
+	if ( defined( 'SPOTIM_IS_VIP_DEBUG' ) && SPOTIM_IS_VIP_DEBUG ) { // Setting WPCOM_IS_VIP_ENV in local won't work.
+		return true;
+	}
 
-    if ( defined( 'WPCOM_IS_VIP_ENV' ) && true === WPCOM_IS_VIP_ENV ) {
-        return true;
-    } else {
-        return false;
-    }
+	if ( defined( 'WPCOM_IS_VIP_ENV' ) && true === WPCOM_IS_VIP_ENV ) {
+		return true;
+	} else {
+		return false;
+	}
 }
