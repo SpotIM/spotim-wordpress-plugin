de164d88fdbb566ab137aa800aa2846693fc701a
Abhijit Rakas <abhijit.rakas@rtcamp.com>
2020-07-28 15:30:52 +0530

    Rename css classes, update button name and test changes

--- templates/admin-template.php
+++ templates/admin-template.php
@@ -1,19 +1,19 @@
-<div class="wrap spotim-page-wrap">
-    <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+<div class="wrap ow-page-wrap">
+	<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
 
-    <nav class="nav-tab-wrapper">
-        <a href="<?php echo esc_url( '?page=' . $this->slug . '&tab=general' ); ?>" class="nav-tab <?php echo ( 'general' === $this->active_tab ) ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'General', 'ow' ); ?></a>
-        <a href="<?php echo esc_url( '?page=' . $this->slug . '&tab=display' ); ?>" class="nav-tab <?php echo ( 'display' === $this->active_tab ) ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Display', 'ow' ); ?></a>
-        <a href="<?php echo esc_url( '?page=' . $this->slug . '&tab=import' ); ?>" class="nav-tab <?php echo ( 'import' === $this->active_tab ) ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Comments Sync', 'ow' ); ?></a>
-        <a href="<?php echo esc_url( '?page=' . $this->slug . '&tab=advanced' ); ?>" class="nav-tab <?php echo ( 'advanced' === $this->active_tab ) ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Advanced', 'ow' ); ?></a>
-        <a href="https://admin.spot.im/" class="nav-tab pull-to-end spotim-dashboard-button" target="_blank"><?php esc_html_e( 'Spot.IM Dashboard', 'ow' ); ?></a>
-    </nav>
+	<nav class="nav-tab-wrapper">
+		<a href="<?php echo esc_url( '?page=' . $this->slug . '&tab=general' ); ?>" class="nav-tab <?php echo ( 'general' === $this->active_tab ) ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'General', 'ow' ); ?></a>
+		<a href="<?php echo esc_url( '?page=' . $this->slug . '&tab=display' ); ?>" class="nav-tab <?php echo ( 'display' === $this->active_tab ) ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Display', 'ow' ); ?></a>
+		<a href="<?php echo esc_url( '?page=' . $this->slug . '&tab=import' ); ?>" class="nav-tab <?php echo ( 'import' === $this->active_tab ) ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Comments Sync', 'ow' ); ?></a>
+		<a href="<?php echo esc_url( '?page=' . $this->slug . '&tab=advanced' ); ?>" class="nav-tab <?php echo ( 'advanced' === $this->active_tab ) ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Advanced', 'ow' ); ?></a>
+		<a href="https://admin.spot.im/" class="nav-tab pull-to-end ow-dashboard-button" target="_blank"><?php esc_html_e( 'OpenWeb.Com Dashboard', 'ow' ); ?></a>
+	</nav>
 
-    <form method="post" action="options.php">
-        <?php
-        settings_fields( $this->option_group );
-        do_settings_sections( $this->slug );
-        submit_button();
-        ?>
-    </form>
+	<form method="post" action="options.php">
+		<?php
+		settings_fields( $this->option_group );
+		do_settings_sections( $this->slug );
+		submit_button();
+		?>
+	</form>
 </div>
