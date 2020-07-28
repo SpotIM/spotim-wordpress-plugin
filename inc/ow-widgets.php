ccca4be80fca242b2669e386dde4756d39c4d469
Abhijit Rakas <abhijit.rakas@rtcamp.com>
2020-07-28 18:09:00 +0530

    Set space indentation

--- inc/ow-widgets.php
+++ inc/ow-widgets.php
@@ -1,7 +1,7 @@
 <?php
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+    exit;
 }
 
 /**
@@ -14,106 +14,106 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class OW_Recirculation_Widget extends WP_Widget {
 
-	/**
-	 * Constructor
-	 *
-	 * Get things started.
-	 *
-	 * @since  4.0.0
-	 *
-	 * @access public
-	 */
-	public function __construct() {
+    /**
+     * Constructor
+     *
+     * Get things started.
+     *
+     * @since  4.0.0
+     *
+     * @access public
+     */
+    public function __construct() {
 
-		parent::__construct(
-			'spotim_recirculation_widget',
-			esc_html__( 'OpenWeb.Com Recirculation', 'ow' ),
-			array(
-				'description' => esc_html__( 'OpenWeb.Com related content.', 'ow' ),
-				'classname'   => 'spotim_recirculation',
-			)
-		);
+        parent::__construct(
+            'spotim_recirculation_widget',
+            esc_html__( 'OpenWeb.Com Recirculation', 'ow' ),
+            array(
+                'description' => esc_html__( 'OpenWeb.Com related content.', 'ow' ),
+                'classname'   => 'spotim_recirculation',
+            )
+        );
 
-	}
+    }
 
-	/**
-	 * Widget
-	 *
-	 * @since  4.0.0
-	 *
-	 * @access public
-	 *
-	 * @return string
-	 */
-	public function widget( $args, $instance ) {
+    /**
+     * Widget
+     *
+     * @since  4.0.0
+     *
+     * @access public
+     *
+     * @return string
+     */
+    public function widget( $args, $instance ) {
 
-		$options = OW_Options::get_instance();
+        $options = OW_Options::get_instance();
 
-		// Ignoring warning as variable is used in included template.
-		$spot_id = $options->get( 'spot_id' ); //phpcs:ignore WordPressVIPMinimum.Variables.VariableAnalysis.UnusedVariable
-		$title   = apply_filters( 'widget_title', empty( $instance['spotim_title'] ) ? '' : $instance['spotim_title'], $instance, $this->id_base );
+        // Ignoring warning as variable is used in included template.
+        $spot_id = $options->get( 'spot_id' ); //phpcs:ignore WordPressVIPMinimum.Variables.VariableAnalysis.UnusedVariable
+        $title   = apply_filters( 'widget_title', empty( $instance['spotim_title'] ) ? '' : $instance['spotim_title'], $instance, $this->id_base );
 
-		// Before widget tag.
-		echo wp_kses_post( $args['before_widget'] );
+        // Before widget tag.
+        echo wp_kses_post( $args['before_widget'] );
 
-		// Title.
-		if ( ! empty( $title ) ) {
-			echo wp_kses_post( $args['before_title'] . $title . $args['after_title'] );
-		}
+        // Title.
+        if ( ! empty( $title ) ) {
+            echo wp_kses_post( $args['before_title'] . $title . $args['after_title'] );
+        }
 
-		// Recirculation.
-		include( plugin_dir_path( dirname( __FILE__ ) ) . 'templates/recirculation-template.php' );
+        // Recirculation.
+        include( plugin_dir_path( dirname( __FILE__ ) ) . 'templates/recirculation-template.php' );
 
-		// After widget tag.
-		echo wp_kses_post( $args['after_widget'] );
+        // After widget tag.
+        echo wp_kses_post( $args['after_widget'] );
 
-	}
+    }
 
-	/**
-	 * Form
-	 *
-	 * @since  4.0.0
-	 *
-	 * @access public
-	 *
-	 * @return void
-	 */
-	public function form( $instance ) {
+    /**
+     * Form
+     *
+     * @since  4.0.0
+     *
+     * @access public
+     *
+     * @return void
+     */
+    public function form( $instance ) {
 
-		// Set default values.
-		$instance = wp_parse_args( (array) $instance, array(
-			'spotim_title' => '',
-		) );
+        // Set default values.
+        $instance = wp_parse_args( (array) $instance, array(
+            'spotim_title' => '',
+        ) );
 
-		// Retrieve an existing value from the database.
-		$spotim_title = ! empty( $instance['spotim_title'] ) ? $instance['spotim_title'] : '';
+        // Retrieve an existing value from the database.
+        $spotim_title = ! empty( $instance['spotim_title'] ) ? $instance['spotim_title'] : '';
 
-		// Form fields.
-		echo '<p>';
-		echo '	<label for="' . esc_attr( $this->get_field_id( 'spotim_title' ) ) . '" class="spotim_title_label">' . esc_html__( 'Title', 'ow' ) . '</label>';
-		echo '	<input type="text" id="' . esc_attr( $this->get_field_id( 'spotim_title' ) ) . '" name="' . esc_attr( $this->get_field_name( 'spotim_title' ) ) . '" class="widefat" value="' . esc_attr( $spotim_title ) . '">';
-		echo '</p>';
+        // Form fields.
+        echo '<p>';
+        echo '	<label for="' . esc_attr( $this->get_field_id( 'spotim_title' ) ) . '" class="spotim_title_label">' . esc_html__( 'Title', 'ow' ) . '</label>';
+        echo '	<input type="text" id="' . esc_attr( $this->get_field_id( 'spotim_title' ) ) . '" name="' . esc_attr( $this->get_field_name( 'spotim_title' ) ) . '" class="widefat" value="' . esc_attr( $spotim_title ) . '">';
+        echo '</p>';
 
-	}
+    }
 
-	/**
-	 * Update
-	 *
-	 * @since  4.0.0
-	 *
-	 * @access public
-	 *
-	 * @return instance
-	 */
-	public function update( $new_instance, $old_instance ) {
+    /**
+     * Update
+     *
+     * @since  4.0.0
+     *
+     * @access public
+     *
+     * @return instance
+     */
+    public function update( $new_instance, $old_instance ) {
 
-		$instance = $old_instance;
+        $instance = $old_instance;
 
-		$instance['spotim_title'] = ( ! empty( $new_instance['spotim_title'] ) ) ? wp_strip_all_tags( $new_instance['spotim_title'] ) : '';
+        $instance['spotim_title'] = ( ! empty( $new_instance['spotim_title'] ) ) ? wp_strip_all_tags( $new_instance['spotim_title'] ) : '';
 
-		return $instance;
+        return $instance;
 
-	}
+    }
 
 }
 
@@ -128,106 +128,106 @@ class OW_Recirculation_Widget extends WP_Widget {
  */
 class OW_Siderail_Widget extends WP_Widget {
 
-	/**
-	 * Constructor
-	 *
-	 * Get things started.
-	 *
-	 * @since  4.2.0
-	 *
-	 * @access public
-	 */
-	public function __construct() {
+    /**
+     * Constructor
+     *
+     * Get things started.
+     *
+     * @since  4.2.0
+     *
+     * @access public
+     */
+    public function __construct() {
 
-		parent::__construct(
-			'spotim_siderail_widget',
-			__( 'OpenWeb.Com Siderail', 'ow' ),
-			array(
-				'description' => __( 'OpenWeb.Com related content.', 'ow' ),
-				'classname'   => 'spotim_siderail',
-			)
-		);
+        parent::__construct(
+            'spotim_siderail_widget',
+            __( 'OpenWeb.Com Siderail', 'ow' ),
+            array(
+                'description' => __( 'OpenWeb.Com related content.', 'ow' ),
+                'classname'   => 'spotim_siderail',
+            )
+        );
 
-	}
+    }
 
-	/**
-	 * Widget
-	 *
-	 * @since  4.2.0
-	 *
-	 * @access public
-	 *
-	 * @return string
-	 */
-	public function widget( $args, $instance ) {
+    /**
+     * Widget
+     *
+     * @since  4.2.0
+     *
+     * @access public
+     *
+     * @return string
+     */
+    public function widget( $args, $instance ) {
 
-		$options = OW_Options::get_instance();
+        $options = OW_Options::get_instance();
 
-		// Ignoring warning as variable is used in included template.
-		$spot_id = $options->get( 'spot_id' ); // phpcs:ignore WordPressVIPMinimum.Variables.VariableAnalysis.UnusedVariable
-		$title   = apply_filters( 'widget_title', empty( $instance['spotim_title'] ) ? '' : $instance['spotim_title'], $instance, $this->id_base );
+        // Ignoring warning as variable is used in included template.
+        $spot_id = $options->get( 'spot_id' ); // phpcs:ignore WordPressVIPMinimum.Variables.VariableAnalysis.UnusedVariable
+        $title   = apply_filters( 'widget_title', empty( $instance['spotim_title'] ) ? '' : $instance['spotim_title'], $instance, $this->id_base );
 
-		// Before widget tag.
-		echo wp_kses_post( $args['before_widget'] );
+        // Before widget tag.
+        echo wp_kses_post( $args['before_widget'] );
 
-		// Title.
-		if ( ! empty( $title ) ) {
-			echo wp_kses_post( $args['before_title'] . $title . $args['after_title'] );
-		}
+        // Title.
+        if ( ! empty( $title ) ) {
+            echo wp_kses_post( $args['before_title'] . $title . $args['after_title'] );
+        }
 
-		// Siderail.
-		include( plugin_dir_path( dirname( __FILE__ ) ) . 'templates/siderail-template.php' );
+        // Siderail.
+        include( plugin_dir_path( dirname( __FILE__ ) ) . 'templates/siderail-template.php' );
 
-		// After widget tag.
-		echo wp_kses_post( $args['after_widget'] );
+        // After widget tag.
+        echo wp_kses_post( $args['after_widget'] );
 
-	}
+    }
 
-	/**
-	 * Form
-	 *
-	 * @since  4.2.0
-	 *
-	 * @access public
-	 *
-	 * @return void
-	 */
-	public function form( $instance ) {
+    /**
+     * Form
+     *
+     * @since  4.2.0
+     *
+     * @access public
+     *
+     * @return void
+     */
+    public function form( $instance ) {
 
-		// Set default values.
-		$instance = wp_parse_args( (array) $instance, array(
-			'spotim_title' => '',
-		) );
+        // Set default values.
+        $instance = wp_parse_args( (array) $instance, array(
+            'spotim_title' => '',
+        ) );
 
-		// Retrieve an existing value from the database.
-		$spotim_title = ! empty( $instance['spotim_title'] ) ? $instance['spotim_title'] : '';
+        // Retrieve an existing value from the database.
+        $spotim_title = ! empty( $instance['spotim_title'] ) ? $instance['spotim_title'] : '';
 
-		// Form fields.
-		echo '<p>';
-		echo '	<label for="' . esc_attr( $this->get_field_id( 'spotim_title' ) ) . '" class="spotim_title_label">' . esc_html__( 'Title', 'ow' ) . '</label>';
-		echo '	<input type="text" id="' . esc_attr( $this->get_field_id( 'spotim_title' ) ) . '" name="' . esc_attr( $this->get_field_name( 'spotim_title' ) ) . '" class="widefat" value="' . esc_attr( $spotim_title ) . '">';
-		echo '</p>';
+        // Form fields.
+        echo '<p>';
+        echo '	<label for="' . esc_attr( $this->get_field_id( 'spotim_title' ) ) . '" class="spotim_title_label">' . esc_html__( 'Title', 'ow' ) . '</label>';
+        echo '	<input type="text" id="' . esc_attr( $this->get_field_id( 'spotim_title' ) ) . '" name="' . esc_attr( $this->get_field_name( 'spotim_title' ) ) . '" class="widefat" value="' . esc_attr( $spotim_title ) . '">';
+        echo '</p>';
 
-	}
+    }
 
-	/**
-	 * Update
-	 *
-	 * @since  4.2.0
-	 *
-	 * @access public
-	 *
-	 * @return instance
-	 */
-	public function update( $new_instance, $old_instance ) {
+    /**
+     * Update
+     *
+     * @since  4.2.0
+     *
+     * @access public
+     *
+     * @return instance
+     */
+    public function update( $new_instance, $old_instance ) {
 
-		$instance = $old_instance;
+        $instance = $old_instance;
 
-		$instance['spotim_title'] = ( ! empty( $new_instance['spotim_title'] ) ) ? wp_strip_all_tags( $new_instance['spotim_title'] ) : '';
+        $instance['spotim_title'] = ( ! empty( $new_instance['spotim_title'] ) ) ? wp_strip_all_tags( $new_instance['spotim_title'] ) : '';
 
-		return $instance;
+        return $instance;
 
-	}
+    }
 
 }
 
@@ -242,7 +242,7 @@ class OW_Siderail_Widget extends WP_Widget {
  * @since 5.0.0 Renamed from `spotim_register_widgets()` to `ow_register_widgets()`
  */
 function ow_register_widgets() {
-	register_widget( 'OW_Recirculation_Widget' );
-	register_widget( 'OW_Siderail_Widget' );
+    register_widget( 'OW_Recirculation_Widget' );
+    register_widget( 'OW_Siderail_Widget' );
 }
 add_action( 'widgets_init', 'ow_register_widgets' );
