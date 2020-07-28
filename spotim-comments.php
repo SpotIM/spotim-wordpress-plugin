004255a17221b301476bc9c81864e3314b071d97
Abhijit Rakas <abhijit.rakas@rtcamp.com>
2020-07-28 18:53:18 +0530

    Set space indentation

--- spotim-comments.php
+++ spotim-comments.php
@@ -14,17 +14,17 @@
 
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+    exit;
 }
 
 // Store Plugin version for internal use.
 if ( ! defined( 'OW_VERSION' ) ) {
-	/**
-	 * The version of the plugin
-	 *
-	 * @since  4.5.2
-	 */
-	define( 'OW_VERSION', '4.5.2' );
+    /**
+     * The version of the plugin
+     *
+     * @since  4.5.2
+     */
+    define( 'OW_VERSION', '4.5.2' );
 }
 
 /**
@@ -37,115 +37,115 @@ if ( ! defined( 'OW_VERSION' ) ) {
  */
 class WP_OW {
 
-	/**
-	 * Instance
-	 *
-	 * @since  1.0.2
-	 *
-	 * @access private
-	 * @static
-	 *
-	 * @var WP_OW
-	 */
-	private static $instance;
+    /**
+     * Instance
+     *
+     * @since  1.0.2
+     *
+     * @access private
+     * @static
+     *
+     * @var WP_OW
+     */
+    private static $instance;
 
-	/**
-	 * Constructor
-	 *
-	 * Get things started.
-	 *
-	 * @since  1.0.2
-	 *
-	 * @access protected
-	 */
-	protected function __construct() {
+    /**
+     * Constructor
+     *
+     * Get things started.
+     *
+     * @since  1.0.2
+     *
+     * @access protected
+     */
+    protected function __construct() {
 
-		// Load plugin files
-		$this->load_files();
+        // Load plugin files
+        $this->load_files();
 
-		// Get the Options
-		$this->options = OW_Options::get_instance();
+        // Get the Options
+        $this->options = OW_Options::get_instance();
 
-		// Run the plugin
-		new OW_i18n();
-		new OW_Cron( $this->options );
-		new OW_Feed();
+        // Run the plugin
+        new OW_i18n();
+        new OW_Cron( $this->options );
+        new OW_Feed();
 
-		if ( is_admin() ) {
+        if ( is_admin() ) {
 
-			// Admin Page
-			new OW_Admin( $this->options );
+            // Admin Page
+            new OW_Admin( $this->options );
 
-		} else {
+        } else {
 
-			// Frontend code: embed script, comments template, comments count.
-			new OW_Frontend( $this->options );
+            // Frontend code: embed script, comments template, comments count.
+            new OW_Frontend( $this->options );
 
-		}
+        }
 
-	}
+    }
 
-	/**
-	 * Get Instance
-	 *
-	 * @since  2.0.0
-	 *
-	 * @access public
-	 * @static
-	 *
-	 * @return WP_OW
-	 */
-	public static function get_instance() {
-		if ( is_null( self::$instance ) ) {
-			self::$instance = new self;
-		}
+    /**
+     * Get Instance
+     *
+     * @since  2.0.0
+     *
+     * @access public
+     * @static
+     *
+     * @return WP_OW
+     */
+    public static function get_instance() {
+        if ( is_null( self::$instance ) ) {
+            self::$instance = new self;
+        }
 
-		return self::$instance;
+        return self::$instance;
 
-	}
+    }
 
-	/**
-	 * Load plugin files
-	 *
-	 * @since  4.3.0 The functionality moved to a method.
-	 *
-	 * @access public
-	 *
-	 * @return void
-	 */
-	public function load_files() {
+    /**
+     * Load plugin files
+     *
+     * @since  4.3.0 The functionality moved to a method.
+     *
+     * @access public
+     *
+     * @return void
+     */
+    public function load_files() {
 
-		$inc_class_dir = plugin_dir_path( __FILE__ ) . DIRECTORY_SEPARATOR . 'inc' . DIRECTORY_SEPARATOR;
+        $inc_class_dir = plugin_dir_path( __FILE__ ) . DIRECTORY_SEPARATOR . 'inc' . DIRECTORY_SEPARATOR;
 
-		$files = [
-			'helpers/class-ow-form.php',
-			'helpers/class-ow-message.php',
-			'helpers/class-ow-comment.php',
-			'helpers/class-ow-json-feed.php',
-			'helpers/class-ow-wp.php',
-			'class-ow-i18n.php',
-			'class-ow-import.php',
-			'class-ow-options.php',
-			'class-ow-settings-fields.php',
-			'class-ow-metabox.php',
-			'class-ow-admin.php',
-			'class-ow-frontend.php',
-			'class-ow-feed.php',
-			'class-ow-cron.php',
-			'ow-shortcodes.php',
-			'ow-widgets.php',
-		];
+        $files = [
+            'helpers/class-ow-form.php',
+            'helpers/class-ow-message.php',
+            'helpers/class-ow-comment.php',
+            'helpers/class-ow-json-feed.php',
+            'helpers/class-ow-wp.php',
+            'class-ow-i18n.php',
+            'class-ow-import.php',
+            'class-ow-options.php',
+            'class-ow-settings-fields.php',
+            'class-ow-metabox.php',
+            'class-ow-admin.php',
+            'class-ow-frontend.php',
+            'class-ow-feed.php',
+            'class-ow-cron.php',
+            'ow-shortcodes.php',
+            'ow-widgets.php',
+        ];
 
-		foreach ( $files as $file ) {
+        foreach ( $files as $file ) {
 
-			$file = $inc_class_dir . $file;
+            $file = $inc_class_dir . $file;
 
-			if ( file_exists( $file ) ) {
-				require_once( $file );
-			}
-		}
+            if ( file_exists( $file ) ) {
+                require_once( $file );
+            }
+        }
 
-	}
+    }
 
 }
 
@@ -158,7 +158,7 @@ class WP_OW {
  * @return WP_OW
  */
 function ow_instance() {
-	return WP_OW::get_instance();
+    return WP_OW::get_instance();
 }
 add_action( 'plugins_loaded', 'ow_instance', 0 );
 
@@ -168,13 +168,13 @@ add_action( 'plugins_loaded', 'ow_instance', 0 );
  * @return bool returns true if current site is available on VIP-GO, otherwise false
  */
 function ow_is_vip() {
-	if ( defined( 'SPOTIM_IS_VIP_DEBUG' ) && SPOTIM_IS_VIP_DEBUG ) { // Setting WPCOM_IS_VIP_ENV in local won't work.
-		return true;
-	}
+    if ( defined( 'SPOTIM_IS_VIP_DEBUG' ) && SPOTIM_IS_VIP_DEBUG ) { // Setting WPCOM_IS_VIP_ENV in local won't work.
+        return true;
+    }
 
-	if ( defined( 'WPCOM_IS_VIP_ENV' ) && true === WPCOM_IS_VIP_ENV ) {
-		return true;
-	} else {
-		return false;
-	}
+    if ( defined( 'WPCOM_IS_VIP_ENV' ) && true === WPCOM_IS_VIP_ENV ) {
+        return true;
+    } else {
+        return false;
+    }
 }
